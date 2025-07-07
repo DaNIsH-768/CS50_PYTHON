740715585e933b3d3559c67c87c6ca9bbbf0437c
Danish Raina <danishraina768@gmail.com>
2025-07-07 01:37:46 +0100

    Created a basic number guessing game.

---
+++ loops_project/main.py
@@ -0,0 +1,32 @@
+import random as rd
+
+GUESSED = []
+guesses = 0
+NUMBER = rd.randint(1, 100)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+    except:
+        print("Invalid number")
+        continue
+
+    if guess in GUESSED:
+        print("Already guessed.")
+        print("guesses:", GUESSED)
+        continue
+
+    GUESSED.append(guess)
+    guesses += 1
+
+    if guess == NUMBER:
+        print(f"You did it. You guessed it in {guesses} guesses.")
+        break
+    else:
+        if guess < NUMBER:
+            print("Too Low.")
+        else:
+            print("Too High")
+
+
+    
